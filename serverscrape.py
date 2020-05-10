17bd2640c5f14aafe7d740b481dd956198441226
zwater <zack@zwater.us>
2020-05-10 04:50:56 +0000

    Does elasticsearch now

--- serverscrape.py
+++ serverscrape.py
@@ -6,18 +6,67 @@ influxHost = 'localhost'
 influxPort = 8086
 influxUser = 'username'
 influxPassword = 'password'
-discordToken = 'your token here' 
+elasticHost = 'localhost'
+
+discordToken = 'your token here'
 databasePrefix = 'serverscrape' # Script will create or use databases named databasePrefix_guildID
 
 #Done with config variables
 
 import discord
+from elasticsearch import Elasticsearch, helpers
 import sys
 from influxdb import InfluxDBClient
 import datetime
+import json
 influx = InfluxDBClient(influxHost, influxPort, influxUser, influxPassword)
+elastic =  Elasticsearch([
+    {'host':elasticHost}
+])
 client = discord.Client()
 guildToScrape = int(sys.argv[1])
+
+def buildElasticDocFromMessage(message):
+    indexName = '{0}_{1}'.format(databasePrefix, message.guild.id)
+    attachments = []
+    for attachment in message.attachments:
+        attachments.append(attachment.url)
+    measurement = 'chatMessage'
+    authorID = message.author.id
+    authorName = message.author.name
+    serverID = message.guild.id
+    serverName = message.guild.name
+    channelID = message.channel.id
+    channelName = message.channel.name
+    messageID = message.id
+    messageTextRaw = message.content
+    messageText = message.clean_content
+    messageLength = len(message.clean_content)
+    timestamp = int((message.created_at - datetime.datetime(1970, 1, 1)).total_seconds() * 1000)
+    createObj  = {
+        "create": {
+            "_index":indexName,
+            "_id":message.id
+        }
+    }
+    docObject = {
+        "type":'default',
+        "timestamp":timestamp,
+        "messageSent":1,
+        "authorID":authorID,
+        "authorName":authorName,
+        "messageID":messageID,
+        "messageLength":messageLength,
+        "messageTextRaw":messageTextRaw,
+        "messageText":messageText,
+        "channelID":channelID,
+        "channelName":channelName,
+        "serverID":serverID,
+        "serverName":serverName,
+        "attachments":', '.join(attachments)
+    }
+    final = '{0}\n{1}'.format(json.dumps(createObj), json.dumps(docObject))
+    return final
 
 def buildInfluxPointFromMessage(message):
     # Grab a bunch of variables to assemble our influx-compatible data point
@@ -83,6 +132,9 @@ async def influxDBInit(guildID):
         print('Created influxDB database {0}'.format(dbName))
     else:
         influx.switch_database(dbName)
+async def elasticInit(guildID):
+    indexName = '{0}_{1}'.format(databasePrefix, guildID)
+    elastic.indices.create(index=indexName, ignore=400)
 
 async def scrapeChannel(channel):
     dbName = '{0}_{1}'.format(databasePrefix, channel.guild.id)
@@ -92,7 +144,8 @@ async def scrapeChannel(channel):
     lastGroup = False
     messageCount = 0
     batches = 0
-    batchBuffer = []
+    influxBatchBuffer = []
+    elasticBatchBuffer = ''
     while lastGroup == False:
         # Grab 1000 messages, before the last message
         # The last message being either the most recent message
@@ -112,7 +165,9 @@ async def scrapeChannel(channel):
             iterator = len(messageBatch)
         for message in messageBatch:
             influxLine = buildInfluxPointFromMessage(message)
-            batchBuffer.append(influxLine)
+            elasticDoc = buildElasticDocFromMessage(message)
+            influxBatchBuffer.append(influxLine)
+            elasticBatchBuffer = '{0}\n{1}'.format(elasticBatchBuffer, elasticDoc)
             messageCount = messageCount + 1
             iterator = iterator - 1
             if iterator == 0:
@@ -121,7 +176,8 @@ async def scrapeChannel(channel):
                 # our 1000 data points to influx in a large batch job.
                 #
                 lastMessage = message
-                influx.write_points(batchBuffer,database=dbName,protocol=u'json')
+                elastic.bulk(elasticBatchBuffer)
+                influx.write_points(influxBatchBuffer,database=dbName,protocol=u'json')
     sys.stdout.write("\n")
     print('{0} Total messages scraped.'.format(messageCount))
 
